dd4ecf3b1127ca2504ac373052eefe1fa80adbe3
srinath6660 <62426826+srinath6660@users.noreply.github.com>
2023-04-27 11:15:51 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stable_diffusion_tf",
+    version="0.1",
+    description="Stable Diffusion in Tensorflow / Keras",
+    author="CapstoneT10",
+    author_email="srinathmintu6660@gmail.com",
+    platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    url="https://github.com/srinath6660/capt10",
+    packages=find_packages(),
+)
